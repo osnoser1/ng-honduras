52db4af41c1637b519dbef421d0736f281224c88
Alfonso Andrés López Molina <alfonso.lopez@merlinjobs.com>
2020-02-23 16:42:01 -0500

    initial commit

---
+++ apps/ngrx-example/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  name: 'ngrx-example',
+  preset: '../../jest.config.js',
+  coverageDirectory: '../../coverage/apps/ngrx-example',
+  snapshotSerializers: [
+    'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
+    'jest-preset-angular/build/AngularSnapshotSerializer.js',
+    'jest-preset-angular/build/HTMLCommentSerializer.js'
+  ]
+};
